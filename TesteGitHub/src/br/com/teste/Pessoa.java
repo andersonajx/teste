f88f0edb943503b233eee7e6a9a393f3b896ee79
andersonajx <andersonajx@gmail.com>
2013-01-17 17:11:33 -0300

    Fiz cagada

--- TesteGitHub/src/br/com/teste/Pessoa.java
+++ TesteGitHub/src/br/com/teste/Pessoa.java
@@ -1,16 +1,10 @@
 package br.com.teste;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import br.com.teste.command.Command;
-import br.com.teste.listener.PessoaListener;
 
 public class Pessoa {
 
 	private String nome;
-	
-	private List<PessoaListener> listeners = new LinkedList<>();
 	
 	private Pessoa(String nome) {
 		this.nome = nome;
@@ -23,22 +17,8 @@ public class Pessoa {
 		return new Pessoa(nome);
 	}
 	
-	public void addPessoaListener(PessoaListener listener) {
-		if (listener == null) {
-			throw new NullPointerException("PessoaListener nao pode ser nulo");
-		}
-		listeners.add(listener);
-	}
-	
 	public void executar(Command<Pessoa> command) {
 		command.execute(this);
-		firePessoaListeners();
-	}
-	
-	private void firePessoaListeners() {
-		for (PessoaListener listener : listeners) {
-			listener.aoExecutarCommand(this);
-		}
 	}
 	
 	public String getNome() {
--- TesteGitHub/src/br/com/teste/listener/PessoaListener.java
+++
@@ -1,9 +0,0 @@
-package br.com.teste.listener;
-
-import br.com.teste.Pessoa;
-
-public interface PessoaListener {
-
-	void aoExecutarCommand(Pessoa pessoa);
-	
-}
