0ca8e8033971a2e8c173773aeb5f5807a8e59471
andersonajx <andersonajx@gmail.com>
2013-01-17 16:20:58 -0300

    Refatorado a classe Pessoa para utilizar os Commands corretamente

--- TesteGitHub/src/br/com/teste/Main.java
+++ TesteGitHub/src/br/com/teste/Main.java
@@ -7,9 +7,9 @@ public final class Main {
 
 	public static void main(String[] args) {
 		Pessoa pessoa = Pessoa.newInstance("Fulano da Silva");
-		pessoa.escreverNome(new EscritaCommand());
-		pessoa.inverterNome(new InverterNomeCommand());
-		pessoa.escreverNome(new EscritaCommand());
+		pessoa.executar(new EscritaCommand());
+		pessoa.executar(new InverterNomeCommand());
+		pessoa.executar(new EscritaCommand());
 	}
 
 }
--- TesteGitHub/src/br/com/teste/Pessoa.java
+++ TesteGitHub/src/br/com/teste/Pessoa.java
@@ -17,11 +17,7 @@ public class Pessoa {
 		return new Pessoa(nome);
 	}
 	
-	public void escreverNome(Command<Pessoa> command) {
-		command.execute(this);
-	}
-	
-	public void inverterNome(Command<Pessoa> command) {
+	public void executar(Command<Pessoa> command) {
 		command.execute(this);
 	}
 	
